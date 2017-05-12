b5c1982ab6ae07fd9800d806cc9a191b67e4c007
Elise St Hilaire <eliseylenore@gmail.com>
2017-05-12 11:59:29 -0700

    Successfully call user info from github

--- src/MyPortfolio/Program.cs
+++ src/MyPortfolio/Program.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace MyPortfolio
 {
@@ -11,14 +13,40 @@ namespace MyPortfolio
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+            var client = new RestClient("https://api.github.com");
+            var request = new RestRequest("users/eliseylenore/repos", Method.GET);
+            request.AddHeader("Accept", "application / vnd.github.v3 + json");
+            client.AddDefaultHeader("User-Agent", "eliseylenore");
+            
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
 
-            host.Run();
+            Console.WriteLine(response.Content);
+            Console.ReadLine();
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+
+            //var host = new WebHostBuilder()
+                //.UseKestrel()
+                //.UseContentRoot(Directory.GetCurrentDirectory())
+                //.UseIISIntegration()
+                //.UseStartup<Startup>()
+                //.Build();
+
+            //host.Run();
         }
     }
-}
+
+
