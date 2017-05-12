c02f8bcba940a93500a52f3111de4113b7c2336e
Elise St Hilaire <eliseylenore@gmail.com>
2017-05-12 13:55:46 -0700

    Move api call to project cclass

---
+++ src/MyPortfolio/Models/Project.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MyPortfolio.Models
+{
+    public class Project
+    {
+        public static JArray GetProjects()
+        {
+            var client = new RestClient("https://api.github.com");
+            var request = new RestRequest("users/eliseylenore/repos", Method.GET);
+            request.AddParameter("sort", "stars");
+            request.AddHeader("Accept", "application / vnd.github.v3 + json");
+            client.AddDefaultHeader("User-Agent", "eliseylenore");
+
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+
+            JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(response.Content);
+            return jsonResponse;
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
--- src/MyPortfolio/Program.cs
+++ src/MyPortfolio/Program.cs
@@ -4,8 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using RestSharp;
-using RestSharp.Authenticators;
+
 
 namespace MyPortfolio
 {
@@ -13,40 +12,18 @@ namespace MyPortfolio
     {
         public static void Main(string[] args)
         {
-            var client = new RestClient("https://api.github.com");
-            var request = new RestRequest("users/eliseylenore/repos", Method.GET);
-            request.AddHeader("Accept", "application / vnd.github.v3 + json");
-            client.AddDefaultHeader("User-Agent", "eliseylenore");
-            
-            var response = new RestResponse();
-            Task.Run(async () =>
-            {
-                response = await GetResponseContentAsync(client, request) as RestResponse;
-            }).Wait();
 
-            Console.WriteLine(response.Content);
-            Console.ReadLine();
-        }
 
-        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
-        {
-            var tcs = new TaskCompletionSource<IRestResponse>();
-            theClient.ExecuteAsync(theRequest, response =>
-            {
-                tcs.SetResult(response);
-            });
-            return tcs.Task;
-        }
+            var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseIISIntegration()
+            .UseStartup<Startup>()
+            .Build();
 
-            //var host = new WebHostBuilder()
-                //.UseKestrel()
-                //.UseContentRoot(Directory.GetCurrentDirectory())
-                //.UseIISIntegration()
-                //.UseStartup<Startup>()
-                //.Build();
-
-            //host.Run();
+            host.Run();
         }
     }
+}
 
 
