4310445cad863cfa74312831637385a56a6fbb95
Elise St Hilaire <eliseylenore@gmail.com>
2017-05-12 16:27:29 -0700

    AJAX looks like should work, but is not.

--- src/MyPortfolio/wwwroot/js/scripts.js
+++ src/MyPortfolio/wwwroot/js/scripts.js
@@ -1,19 +1,22 @@
-﻿$(document).ready(function() {
-    $.ajax({
-        type: 'GET',
-        url: '@Url.Action("/Projects/GetProjects")',
-        dataType: 'json',
-        complete: function (result) {
-            console.log(result);
-            result.forEach(function (project) {
-                $(".projects").append(
-                "<div class='project-display'>" +
-                "<h1>" +
-                project.name +
-                "</h1>" +
-                "</div>"
-                );
-            });
-        }
+﻿$(document).ready(function () {
+    $(".click-for-projects").click(function () {
+        console.log("HEYYO");
+        $.ajax({
+            type: 'GET',
+            url: $(this).data('request-url'),
+            dataType: 'json',
+            complete: function (result) {
+                console.log(result);
+                result.forEach(function (project) {
+                    $(".projects").append(
+                    "<div class='project-display'>" +
+                    "<h1>" +
+                    project.name +
+                    "</h1>" +
+                    "</div>"
+                    );
+                });
+            }
+        });
     });
 });
