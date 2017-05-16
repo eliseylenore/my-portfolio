71653d28d1a9c308e947fc430c35e43506e842d2
Elise St Hilaire <eliseylenore@gmail.com>
2017-05-16 14:41:25 -0700

    Create functionality to click and see spiel

--- src/MyPortfolio/wwwroot/js/scripts.js
+++ src/MyPortfolio/wwwroot/js/scripts.js
@@ -9,15 +9,21 @@
                 console.log(result);
                 console.log("result.responseJSON[0]: " + result.responseJSON[0]);
                 console.log("result.responseJSON[0].language: " + result.responseJSON[0].language);
-                for (var i = 0; i < 4; i++) {
+                $(".projects").text("");
+                for (var i = 0; i < 3; i++) {
                     $(".projects").append(
                     "<div class='project-display'>" +
                     "<h1>" +
                     result.responseJSON[i].name +
                     "</h1>" +
-                    "</div>")
+                    "</div>")                   
                 }
+                $(".spiel").hide();
                 }
             });
-        });
+    });
+    $(".home-button").click(function () {
+        $(".spiel").show();
+        $(".projects").hide();
+    })
   });
